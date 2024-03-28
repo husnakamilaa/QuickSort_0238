d7d943d101a374ffb380d9e55b5cc71222334436
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-03-28 14:46:24 +0700

    membuat variabel  global

--- QuickSort_0238.cpp
+++ QuickSort_0238.cpp
@@ -4,6 +4,12 @@
 #include <iostream>
 using namespace std;
 
+//array of integers to hold values
+int arr[20];
+int cmp_count = 0; //number of comparison
+int mov_count = 0; //number of data movements
+int n;
+
 int main()
 {
     std::cout << "Hello World!\n";
