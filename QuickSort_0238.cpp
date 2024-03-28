985391337f62854c923ecf07f479ca91b30b0867
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-03-28 14:52:29 +0700

    membuat perulangan while didalam prosedur input

--- QuickSort_0238.cpp
+++ QuickSort_0238.cpp
@@ -10,6 +10,19 @@ int cmp_count = 0; //number of comparison
 int mov_count = 0; //number of data movements
 int n;
 
+void input() {
+    while (true)
+    {
+        cout << "Masukkan panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
